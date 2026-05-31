bfef5951531fbbab2e78bc30bf5744c45423d4b4
Leteziy <lizakazarskaa@gmail.com>
2026-05-31 23:11:58 +0300

    Изменен селектор для поиска кнопки Start и клика по ней

--- lesson_06/lesson06_task1.py
+++ lesson_06/lesson06_task1.py
@@ -1,22 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
-start = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((By.ID, "start"))
+start_button = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "#start button"))
 )
-start.click()
+start_button.click()
 WebDriverWait(driver, 30).until(
     EC.text_to_be_present_in_element((By.ID, "finish"), "Hello World!")
 )
-finish = driver.find_element(By.ID, "finish")
 driver.save_screenshot("result.png")
-assert (
-    finish.text == "Hello World!"
-)
-print(finish.text)
+finish = driver.find_element(By.ID, "finish")
+assert finish.text == "Hello World!"
 driver.quit()
