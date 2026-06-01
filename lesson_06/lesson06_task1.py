0982d9b4646991a72a76bb07191c9b0698ae585b
Leteziy <lizakazarskaa@gmail.com>
2026-06-01 23:15:59 +0300

    Добавлены функции def

--- lesson_06/lesson06_task1.py
+++ lesson_06/lesson06_task1.py
@@ -4,17 +4,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
-start_button = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "#start button"))
-)
-start_button.click()
-WebDriverWait(driver, 30).until(
-    EC.text_to_be_present_in_element((By.ID, "finish"), "Hello World!")
-)
-driver.save_screenshot("result.png")
-finish = driver.find_element(By.ID, "finish")
-assert finish.text == "Hello World!"
-driver.quit()
+def test_dynamic_loading():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
+
+    start_button = WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#start button"))
+    )
+    start_button.click()
+
+    WebDriverWait(driver, 30).until(
+        EC.text_to_be_present_in_element((By.ID, "finish"), "Hello World!")
+    )
+
+    driver.save_screenshot("result.png")
+
+    finish = driver.find_element(By.ID, "finish")
+    assert finish.text == "Hello World!"
+
+    driver.quit()
